bf91cbcfd2729a6898983e31f322ba3b77dbc374
james <jamesconcepciongit@gmail.com>
2025-05-19 15:58:43 +0800

    Updates

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="bg-red-400 text-center py-4">
-            <p className="text-gray-600 text-sm">&copy; {new Date().getFullYear()} James Portfolio. All rights reserved.</p>
+            <p className="text-gray-800 text-sm">&copy; {new Date().getFullYear()} James Portfolio. All rights reserved.</p>
         </footer>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,19 @@ import Skills from './skills/Skills'
 export default function Home() {
     return (
       <>
+        <main className="min-h-screen flex flex-col items-center justify-center bg-red-50 from-current">
         <div className="sticky top-0 z-50">
           <Navbar />
         </div>
-        <main className="min-h-screen flex flex-col items-center justify-center bg-red-50 from-current">
           <h1 className="text-4xl font-bold mb-4">---------</h1>
           <p className="text-lg text-gray-700">Welcome to my Next.js portfolio!</p>
-        </main>
+
         <About />
         <Contact />
         <Projects />
         <Skills />
         <Footer />
+        </main>
       </>
     );
   }
