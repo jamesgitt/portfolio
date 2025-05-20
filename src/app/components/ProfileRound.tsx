d0d28c62e6307f7a8fe4b37b1c009144c868253e
james <jamesconcepciongit@gmail.com>
2025-05-20 18:10:49 +0800

    Home Page Updates

--- src/app/components/ProfileRound.tsx
+++ src/app/components/ProfileRound.tsx
@@ -1,10 +1,14 @@
+import Image from 'next/image';
+
 export default function ProfileRound() {
     return (
         <div className="justify-center items-center py-8">
             <div className="w-[38rem] h-[48rem] rounded-full overflow-hidden border-4 border-black shadow flex items-center justify-center drop-shadow-xl">
-                <img
+                <Image
                     src="/images/ProfilePicture.jpg"
                     alt="Profile"
+                    width={608} // 38rem * 16 = 608px
+                    height={768} // 48rem * 16 = 768px
                     className="w-full h-full object-cover"
                     style={{ display: 'block' }}
                 />
